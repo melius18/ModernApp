ba8e3fcedda3767a580b7486aa0c657c6ef372e8
melius18 <melius18@protonmail.com>
2018-06-25 22:48:58 +0900

    apply service instance

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,20 @@
 var app = angular.module('app', []);
 
-app.controller('PostsCtrl', function ($scope, $http) {
+app.service('PostsSvc', function ($http) {
+    this.fetch = function () {
+        return $http.get('/api/posts');
+    }
+
+    this.create = function (post) {
+        return $http.post('/api/posts', post);
+    }
+});
+
+app.controller('PostsCtrl', function ($scope, PostsSvc) {
 
     $scope.addPost = function () {
         if ($scope.postBody) {
-            $http.post('/api/posts', {
+            PostsSvc.create({
                 username: 'rcode',
                 body: $scope.postBody
             }).success(function (post) {
@@ -15,8 +25,7 @@ app.controller('PostsCtrl', function ($scope, $http) {
     }
 
     // $scope.posts = [];
-    $http.get('/api/posts')
-        .success(function (posts) {
-            $scope.posts = posts;
-        });
+    PostsSvc.fetch().success(function (posts) {
+        $scope.posts = posts;
+    });
 });

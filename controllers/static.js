11b662e301c213b4f50846d24836cda1a878e8b8
melius18 <melius18@protonmail.com>
2018-06-25 21:13:13 +0900

    separate static file

--- controllers/static.js
+++ controllers/static.js
@@ -1,7 +1,14 @@
-var router = require('express').Router();
+var express = require('express');
+var router = express.Router();
+
+router.use(express.static(__dirname + '/../assets'));   // folder for static asset
+console.log('static.js 1: ' + __dirname);
+console.log('static.js 1: ' + process.cwd());
 
 router.get('/', function (req, res) {
     res.sendfile('layouts/posts.html');
+    console.log('static.js 2: ' + __dirname);
+    console.log('static.js 2: ' + process.cwd());
 });
 
 module.exports = router;

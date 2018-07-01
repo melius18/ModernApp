ae369b50e5654b2a5c1695a2b4eb6ce3bcf2b43e
melius18 <melius18@example.com>
2018-07-01 11:25:45 +0900

    gulp uglify & annotate

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
+var ngAnnotate = require('gulp-ng-annotate');
 
 gulp.task('hello', ['js'], function () {
     console.log('hello gulp');
@@ -9,6 +10,7 @@ gulp.task('hello', ['js'], function () {
 gulp.task('js', function () {
     gulp.src(['ng/module.js', 'ng/**/*.js'])
     .pipe(concat('app.js'))
+    .pipe(ngAnnotate())
     .pipe(uglify())
     .pipe(gulp.dest('assets'));
 });
